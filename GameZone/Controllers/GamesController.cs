242894e9c5537fe96ff652c33c13f507e08f9761
Mahmoud El-Sayed <me974931@gmail.com>
2024-05-09 12:24:14 +0300

    Add Form Inputs

--- GameZone/Controllers/GamesController.cs
+++ GameZone/Controllers/GamesController.cs
@@ -1,17 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using GameZone.Data;
+using GameZone.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace GameZone.Controllers
 {
     public class GamesController : Controller
     {
-        public IActionResult Index()
+        private readonly AppDbContext _context;
+
+		public GamesController(AppDbContext context)
+		{
+			_context = context;
+		}
+
+		public IActionResult Index()
         {
             return View();
         }
 
         public IActionResult Create()
         {
-            return View();
+            var cats= _context.Categories.ToList();
+            var devs= _context.Devices.ToList();
+
+			var model = new CreateGameFormViewModel
+            {
+                Categories=cats.Select(c=>new SelectListItem(value:c.Id.ToString(),text:c.Name)).OrderBy(c=>c.Text).ToList(),
+                Devices=devs.Select(c=>new SelectListItem(value: c.Id.ToString(), text: c.Name)).OrderBy(c => c.Text).ToList(),
+			};
+
+			return View(model);
         }
     }
 }
