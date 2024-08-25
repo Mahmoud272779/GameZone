491e35857e8eb2d13917acfc2d2fd2fbc5830d9b
Mahmoud El-Sayed <me974931@gmail.com>
2024-08-25 17:17:56 +0300

    fs

--- GameZone/obj/Debug/net7.0/GameZone.AssemblyInfo.cs
+++ GameZone/obj/Debug/net7.0/GameZone.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameZone")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4d74f7b8239a7e3b5ad79651dde91cc9a64fd0be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f721556a00c4e433f6fc8deb6970fd280b69d54")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameZone")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameZone")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
